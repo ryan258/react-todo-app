763e55b226790c95886c37ebf20c41e53e5af8f2
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-06-26 14:26:58 -0500

    Handle toggling through state.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function TodoItem(props) {
+function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.todo.completed && true} />
-      <p> {props.todo.text}</p>
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p>{props.item.text}</p>
     </div>
   );
 }
+
+export default TodoItem;
